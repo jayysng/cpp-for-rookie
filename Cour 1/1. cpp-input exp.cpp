c26b6657056fee2518d807e68e94baa3fd96044d
Ananda Jaya Sinaga <140717428+jayysng@users.noreply.github.com>
2023-12-18 12:59:12 +0700

    Create cpp-input exp.cpp

---
+++ Cour 1/1. cpp-input exp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+    // cin - console input
+    int a;
+    cin >> a;
+    cout << "Variable name a = " << a << endl;
+
+    string name,surname;
+
+    cout << "Masukkan nama depan : ";
+    cin >> name;
+
+    cout << "Masukkan nama belakang : ";
+    cin >> surname;
+
+    cout << "Selamat datang "<<name<<" "<<surname<<endl;
+
+}
